f6b21f167166033a57dfb790a1855ee29b9af303
underwaterhayk <kirakosjorob@gmail.com>
2018-07-26 16:35:19 +0500

    initial commit

---
+++ desktop/src/com/rockbite/inetrnship/ghosthouse/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rockbite.inetrnship.ghosthouse.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rockbite.inetrnship.ghosthouse.GhostHouse;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GhostHouse(), config);
+	}
+}
