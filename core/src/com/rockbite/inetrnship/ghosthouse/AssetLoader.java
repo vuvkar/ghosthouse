21157b134265287c217fd20805e051bd5b250cf1
davekirakosyan <dav.kyan@gmail.com>
2018-07-30 20:16:18 +0400

    Added Image Reader

--- core/src/com/rockbite/inetrnship/ghosthouse/AssetLoader.java
+++ core/src/com/rockbite/inetrnship/ghosthouse/AssetLoader.java
@@ -19,10 +19,49 @@ public class AssetLoader extends AssetManager {
         rooms = processRoomData(pixelData);
     }
 
-    public int[] readPixelData() {
-        // TODO: courtesy of Dave
+    public int[][] readPixelData() {
+        int[][] matrix = new int[0][0];
+        String filenmae = "unt.pbm";
+        String line = null;
+        try {
+            FileReader filereader = new FileReader("unt.pbm");
+            BufferedReader bufferedreader = new BufferedReader(filereader);
+            line = bufferedreader.readLine();
+            assert line != "P1";
+            line = bufferedreader.readLine();
+            while (line.charAt(0) == '#') {
+                line = bufferedreader.readLine();
+            }
+            java.lang.String[] tmp = line.split(" ");
+            int width = Integer.parseInt(tmp[0]);
+            int height = Integer.parseInt(tmp[1]);
 
-        return null;
+            matrix = new int[width][height];
+            int i = 0;
+            int j = 0;
+            while (i < height - 1 && j < width - 1) {
+                line = bufferedreader.readLine();
+                while (line.charAt(0) == '#') {
+                    line = bufferedreader.readLine();
+                }
+                tmp = line.split(" ");
+                int k = 0;
+                while (k < tmp.length) {
+                    matrix[i][j] = Integer.parseInt(tmp[k]);
+                    k++;
+                    if (j + 1 < width) {
+                        j++;
+                    } else {
+                        j = 0;
+                        i++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("dsg");
+        }
+        return (matrix) ;
     }
 
 
