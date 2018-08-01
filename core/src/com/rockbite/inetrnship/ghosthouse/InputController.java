ae746c641c0a80811f5c394dd9044adab13ec8a8
wtflian <lharutyunyan99@gmail.com>
2018-08-01 16:27:57 +0400

    Added InputController.java

---
+++ core/src/com/rockbite/inetrnship/ghosthouse/InputController.java
@@ -0,0 +1,13 @@
+package com.rockbite.inetrnship.ghosthouse;
+
+public class InputController {
+    // TODO: Monika's shit
+    public void getInputCoordinates() {
+
+    }
+
+    // TODO: Liana's MEGAshit
+    public void moveCharacter() {
+
+    }
+}
