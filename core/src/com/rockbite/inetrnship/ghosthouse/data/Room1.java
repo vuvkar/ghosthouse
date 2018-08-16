3c513f5a90b8fc3e09e4d5068c28987f0989f257
davekirakosyan <dav.kyan@gmail.com>
2018-08-16 15:18:47 +0400

    Room 2 Logic

--- core/src/com/rockbite/inetrnship/ghosthouse/data/Room1.java
+++ core/src/com/rockbite/inetrnship/ghosthouse/data/Room1.java
@@ -121,8 +121,6 @@ public class Room1 extends Room {
                     case STATIC:
                         break;
                     case TAKEABLE:
-
-
                         changeTexture(BOTTLES,"bottlered1room1");
                         addToInventory(BOTTLES);
                         changeTexture(BOTTLES,"bottlesredroom1");
