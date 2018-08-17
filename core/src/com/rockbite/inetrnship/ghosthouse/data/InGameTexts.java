72f60947d2a1ae9b898cd14dacdf3dbb0553a186
linismona <liliane.demirdjian@rockbitegames.com>
2018-08-17 14:43:40 +0400

    3roomtrtrikner

--- core/src/com/rockbite/inetrnship/ghosthouse/data/InGameTexts.java
+++ core/src/com/rockbite/inetrnship/ghosthouse/data/InGameTexts.java
@@ -33,8 +33,8 @@ public class InGameTexts
     // static room2
     public static String startr1 = "Yei, the door is open here!";
     public static String startr2 = "Let's go!";
-    public static String bug1 = "AAAAA...My biggest fear";
-    public static String bug3 = "I need to find a way to kill them, otherwise I'm stuck here";
+    public static String bug1 = "AAAAA...Insects! My biggest fear !";
+    public static String bug3 = "I need to find a way to kill it, otherwise I'm stuck here";
     public static String paper1 = "Anti-Bug remedy? hmm...let's see...";
     public static String paper2 = "we'll need to mix couple of ingredients into a spray bottle";
     public static String spray = "We'll have to mix all the ingredients inside";
@@ -48,4 +48,15 @@ public class InGameTexts
     public static String sekator = "This will help for the most important ingredient";
     public static String recipe = "I don't know the recipe";
     public static String spraydone = "Now I can get out of here fearlessly";
+ // static room3
+    public static String room3 = "There it is. I can kill him easily";
+    public static String aftermingame = "Huh, I can safely explore the house now";
+    public static String doortp = "Again this door is closed???";
+    public static String doortp1 = "Let's see how can I get out now";
+    public static String tapor1 = "If only I can find something to fix";
+    public static String tapor2 = "Hope the other part is here";
+    public static String kle = "I guess this will work ";
+    public static String tapormer = "ready? this will require a lot of strength";
+    public static String kletapor = "yeii";
 }
+
--- core/src/com/rockbite/inetrnship/ghosthouse/data/Room3.java
+++ core/src/com/rockbite/inetrnship/ghosthouse/data/Room3.java
@@ -1,5 +1,8 @@
 package com.rockbite.inetrnship.ghosthouse.data;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.rockbite.inetrnship.ghosthouse.DialogSystem;
 import com.rockbite.inetrnship.ghosthouse.MiniGames.KillTheBugs.KillBugs;
 
 public class Room3 extends Room {
@@ -16,9 +19,27 @@ public class Room3 extends Room {
     final int MAGI_POLKA = 25;
     final int VEREVI_POLKA = 26;
 
+    Sound tapor1;
+    Sound tapor2;
+    Sound kle;
+    Sound jardel;
+    Sound box;
+    Sound wholekey;
+
+
+
+
 
     @Override
     public void roomStarted() {
+        tapor1 = Gdx.audio.newSound (Gdx.files.internal("sounds/tapor.mp3"));
+        tapor2 = Gdx.audio.newSound (Gdx.files.internal("sounds/tapor.mp3"));
+        kle = Gdx.audio.newSound(Gdx.files.internal("sounds/kle.mp3"));
+        jardel = Gdx.audio.newSound(Gdx.files.internal("sounds/jardel.mp3"));
+        box = Gdx.audio.newSound(Gdx.files.internal("sounds/box.mp3"));
+        wholekey = Gdx.audio.newSound(Gdx.files.internal("sounds/sparkle.mp3"));
+
+        DialogSystem.dialogSystem.startDialog(InGameTexts.room3, 1.5f, 0.5f, 0f);
         moveGhostTo(DOOR_LEFT);
         setItemStatus(BUG, ItemType.NONTAKEABLE);
     }
@@ -33,6 +54,14 @@ public class Room3 extends Room {
                     case TAKEABLE:
                         setItemStatus(MAGI_POLKA, ItemType.STATIC);
                         addToInventory(TAPOR_PART1);
+                        tapor1.play(Room.soundVolume);
+                        if (getItemStatus(TAPOR_PART2) == ItemType.STATIC)
+                        {
+                            DialogSystem.dialogSystem.startDialog(InGameTexts.tapor1, 1.5f, 0.5f, 0f);
+                        } else {
+                            DialogSystem.dialogSystem.startDialog(InGameTexts.tapor1, 2f, 0.5f, 0f);
+                        }
+                        setItemStatus(TAPOR_PART1, ItemType.STATIC);
                         break;
                     case NONTAKEABLE:
                         break;
@@ -45,6 +74,15 @@ public class Room3 extends Room {
                     case TAKEABLE:
                         setItemStatus(TUMBCHKA, ItemType.STATIC);
                         addToInventory(TAPOR_PART2);
+                        tapor2.play(Room.soundVolume);
+                        DialogSystem.dialogSystem.startDialog(InGameTexts.tapor1, 1.5f, 0.5f, 0f);
+                        if (getItemStatus(TAPOR_PART1) == ItemType.STATIC) {
+                            DialogSystem.dialogSystem.startDialog(InGameTexts.tapor2, 1.5f, 0.5f, 0f);
+                        } else {
+                            DialogSystem.dialogSystem.startDialog(InGameTexts.tapor1, 2f, 0.5f, 0f);
+                        }
+                        setItemStatus(TAPOR_PART2, ItemType.STATIC);
+
                         break;
                     case NONTAKEABLE:
                         break;
@@ -69,6 +107,8 @@ public class Room3 extends Room {
                     case TAKEABLE:
                         setItemStatus(VEREVI_POLKA, ItemType.STATIC);
                         addToInventory(GLUE);
+                        box.play(Room.soundVolume);
+                        DialogSystem.dialogSystem.startDialog(InGameTexts.kle, 1.5f, 0.5f, 0f);
                         break;
                     case NONTAKEABLE:
                         break;
@@ -82,6 +122,7 @@ public class Room3 extends Room {
     public void itemWasDragged(int fromInventory, int toRoomItem) {
         if (fromInventory == TAPOR && toRoomItem == DOOR) {
             removeFromInventory(TAPOR);
+            jardel.play(Room.soundVolume);
             changeTexture(DOOR, "");
             leaveRoom();
         }
@@ -90,6 +131,7 @@ public class Room3 extends Room {
     @Override
     public void itemWasMoved(int fromInventory, int toInventoryItem) {
         if ((fromInventory == TAPOR_PART1 && toInventoryItem == GLUE) || (fromInventory == GLUE && toInventoryItem == TAPOR_PART1)) {
+            kle.play(Room.soundVolume);
             removeFromInventory(fromInventory);
             removeFromInventory(toInventoryItem);
             addToInventory(GLUED_TAPOR);
@@ -97,6 +139,7 @@ public class Room3 extends Room {
 
         if ((fromInventory == GLUED_TAPOR && toInventoryItem == TAPOR_PART2) || (fromInventory == TAPOR_PART2 && toInventoryItem == GLUED_TAPOR)) {
             removeFromInventory(fromInventory);
+            wholekey.play(Room.soundVolume);
             removeFromInventory(toInventoryItem);
             addToInventory(TAPOR);
         }
@@ -106,6 +149,7 @@ public class Room3 extends Room {
     public void miniGameWasClosed(boolean hasWon) {
         if(hasWon) {
             changeTexture(BUG, "");
+            DialogSystem.dialogSystem.startDialog(InGameTexts.aftermingame, 1.5f, 0.5f, 0f);
             //removeFromInventory(Room2.SPRAY_BOTTLE);
             setItemStatus(TUMBCHKA, ItemType.TAKEABLE);
             setItemStatus(VEREVI_POLKA, ItemType.TAKEABLE);
